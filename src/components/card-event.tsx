f0b3a2c6d4771dc6b24584ee08e98bd8b37d12c5
Gabriel Felipe Buttendorf <bielbuttendorf@gmail.com>
2024-04-05 21:09:18 -0300

    event search feature

--- src/components/card-event.tsx
+++ src/components/card-event.tsx
@@ -1,12 +1,16 @@
 interface CardEventProps {
   description: string
   children: string
+  attendees: number
 }
 
 export function CardEvent(props: CardEventProps) {
   return (
     <div className="w-full rounded-lg bg-zinc-900 flex flex-col gap-2 p-4">
-      <h1 className="text-lg font-semibold">{props.children}</h1>
+      <div className="flex gap-4 items-center">
+        <h1 className="text-lg font-semibold">{props.children}</h1>
+        <span className="text-xs text-emerald-300">{props.attendees} participantes</span>
+      </div>
       <span className="text-sm">{props.description}</span>
     </div>
   )
--- src/components/event-list.tsx
+++ src/components/event-list.tsx
@@ -1,8 +1,60 @@
-import { Search } from "lucide-react";
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, Search } from "lucide-react";
 import { Table } from "./table/table"
 import { CardEvent } from "./card-event";
+import { ChangeEvent, useEffect, useState } from "react"
+import { IconButton } from "./icon-button";
+
+const eventsData = [
+  {
+    name: "NLW Unite", 
+    attendees: Math.floor(Math.random() * 250), 
+    description: "O NLW Unite da Rocketseat foi um evento online e gratuito que aconteceu em abril de 2024. O objetivo do evento era desafiar programadores de todos os níveis a criarem um projeto completo em 3 aulas, utilizando uma nova tecnologia." 
+  },
+  {
+    name: "NLW Expert", 
+    attendees: Math.floor(Math.random() * 250), 
+    description: "O NLW Expert da Rocketseat é um programa de mentoria exclusivo para desenvolvedores experientes que desejam elevar suas habilidades e alcançar o próximo nível em suas carreiras." 
+  },
+  {
+    name: "Explorer", 
+    attendees: Math.floor(Math.random() * 250), 
+    description: "O Explorer da Rocketseat é um programa completo de formação para quem deseja iniciar na área de programação. Através do método 7-2-1 (70% prática, 20% networking e 10% branding), o programa te prepara para se tornar um desenvolvedor FullStack completo, pronto para conquistar sua primeira vaga no mercado." 
+  }
+]
 
 export function EventList() {
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has("search")) {
+      return url.searchParams.get("search") ?? ''
+    }
+
+    return ''
+  })
+
+  const [filteredEvents, setFilteredEvents] = useState(eventsData)
+  useEffect(() => {
+    const filtered = eventsData.filter(event => 
+      event.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    )
+    setFilteredEvents(filtered)
+  }, [search])
+
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set("search", search)
+    
+    window.history.pushState({}, "", url)
+
+    setSearch(search)
+  }
+
+  function onChangeSearchInput(event: ChangeEvent<HTMLInputElement>) {
+    setCurrentSearch(event.target.value)
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
@@ -10,6 +62,8 @@ export function EventList() {
         <div className="w-72 px-3 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
           <Search className="size-4 text-emerald-300" />
           <input type="text"
+            value={search}
+            onChange={onChangeSearchInput}
             className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"   
             placeholder="Buscar evento..." 
           />
@@ -17,15 +71,15 @@ export function EventList() {
       </div>
 
       <Table>
-        <div className="flex flex-col gap-2 p-1">
-          <CardEvent 
-            description="Este é o resumo do NLW Unite">
-            NLW Unite
-          </CardEvent>
-          <CardEvent 
-            description="Este é o resumo do NLW Expert">
-            NLW Expert
-          </CardEvent>
+        <div className="flex flex-col gap-5 px-2 py-3">
+          {filteredEvents.map((event, index) => (
+            <CardEvent 
+              key={index}
+              attendees={event.attendees}
+              description={event.description}>
+              {event.name}
+            </CardEvent>
+          ))}
         </div>
       </Table>
     </div>
